1ee909273f481a9daa3111fa880b418eabbc97f4
Tatarkina Polina <tatarkina.03@list.ru>
2025-02-06 00:12:03 +0500

    third iteration

--- ftracker.go
+++ ftracker.go
@@ -102,8 +102,8 @@ const (
 // height float64 — рост пользователя.
 func WalkingSpentCalories(action int, duration, weight, height float64) float64 {
 	// ваш код здесь
-	Speed := meanSpeed(action, duration) * kmhInMsec
-	return ((walkingCaloriesWeightMultiplier*weight + (math.Pow(Speed, 2))/height) * walkingSpeedHeightMultiplier * weight) * duration * minInH
+	speed := meanSpeed(action, duration) * kmhInMsec
+	return ((walkingCaloriesWeightMultiplier * weight) + ((math.Pow(speed, 2) / height) * walkingSpeedHeightMultiplier * weight)) * duration * minInH
 }
 
 // Константы для расчета калорий, расходуемых при плавании.
